b467f219b79e53dd16da1fec2a147eba7fce4c08
Jack Cook <jack@jackcook.nyc>
2015-01-30 23:00:00 -0500

    Create ConfirmViewController

---
+++ BioMessage/views/ConfirmViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ConfirmViewController.swift
+//  BioMessage
+//
+//  Created by Jack Cook on 1/30/15.
+//  Copyright (c) 2015 Jack Cook. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
