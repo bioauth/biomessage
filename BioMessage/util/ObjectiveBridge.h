a4ffee84a81450a40634861a3ac643aead59dbe0
Jack Cook <jack@jackcook.nyc>
2015-01-30 23:56:03 -0500

    Finished visuals for ConfirmViewController

---
+++ BioMessage/util/ObjectiveBridge.h
@@ -0,0 +1,9 @@
+//
+//  ObjectiveBridge.h
+//  BioMessage
+//
+//  Created by Jack Cook on 1/30/15.
+//  Copyright (c) 2015 Jack Cook. All rights reserved.
+//
+
+#import "ChameleonFramework/Chameleon.h"
--- BioMessage/views/ConfirmViewController.swift
+++ BioMessage/views/ConfirmViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ConfirmViewController: UIViewController {
 
+    @IBOutlet var readyButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = UIColor(gradientStyle: UIGradientStyle.TopToBottom, withFrame: self.view.frame, andColors: [UIColor.flatLimeColorDark(), UIColor.flatLimeColor()])
+        
+        readyButton.setBackgroundImage(UIImage(named: "white.png"), forState: .Normal)
+        readyButton.setBackgroundImage(UIImage(named: "gray.png"), forState: .Highlighted)
+        
+        readyButton.layer.cornerRadius = 8
+        readyButton.clipsToBounds = true
+        
+        readyButton.setTitleColor(UIColor(red: 0.56, green: 0.7, blue: 0.11, alpha: 1), forState: .Normal)
+        readyButton.titleLabel?.font = UIFont(name: "Avenir-Roman", size: 28)
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
     }
 }
